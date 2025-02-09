14ef67dc80fb120c84dbfe875521120a639ef4da
TebogoMdladla <txbzatxbogo@gmail.com>
2025-02-09 21:07:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    const messages = [
+        "Hi Baby ",
+        "Hope you're having a great day, because you deserve it! 😊",
+        "You're an amazing person. Stay blessed and keep being you. 🌟"
+        "P.S text me back bathong! "
+    ];
+
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    document.getElementById("message").innerText = randomMessage;
+}
